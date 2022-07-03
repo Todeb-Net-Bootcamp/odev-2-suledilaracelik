738ba5a120692e0d8b559ca88774d7c1a7866779
Şule Dilara Çelik <suledilara21@gmail.com>
2022-07-03 13:57:22 +0300

    homework2

---
+++ homework-2/Abstract/Araba.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework_2.Abstract
+{
+    public abstract class Araba
+    {
+        //base classta özelliklerimi oluşturdum.
+        public string renk = "siyah";
+        public string yakit_turu = "benzin";
+        public string vites_turu = "otomatik";
+        private string model_yili = "1999";
+        public abstract void motor_gucu_hesapla();
+
+        public virtual void parksensörü()
+        {
+            Console.WriteLine("Park sensörü vardır.");
+        }
+
+        public string ad
+        {
+            get
+            {
+                return model_yili;
+            }
+            set
+            {
+                model_yili = value;
+            }
+        }
+
+    }
+}
